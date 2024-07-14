f95b3e092a9774f4c7b092b11ef640c40d28700b
Kelv1nG <kelvinmaglipon@gmail.com>
2024-07-14 14:06:05 +0800

    set formatting options for languages used

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local set_autoformat = function(pattern, bool_val)
+  vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = pattern,
+    callback = function()
+      vim.b.autoformat = bool_val
+    end,
+  })
+end
+
+set_autoformat({ "php" }, false)
+set_autoformat({ "python" }, true)
+set_autoformat({ "go" }, true)
